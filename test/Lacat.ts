32eb77e6783fe9109097b7afb6ab76fc03e934f8
Adam Balogh <balogh.adam@icloud.com>
2022-09-08 18:17:49 +0400

    test

--- test/Lacat.ts
+++ test/Lacat.ts
@@ -3,122 +3,66 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Lock", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    const ONE_GWEI = 1_000_000_000;
+describe("Lacat", function () {
 
-    const lockedAmount = ONE_GWEI;
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
 
-    // Contracts are deployed using the first signer/account by default
+  async function deployLacat() {
     const [owner, otherAccount] = await ethers.getSigners();
 
-    const Lock = await ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const Lacat = await ethers.getContractFactory("Lacat");
+    const lacat = await Lacat.deploy();
 
-    return { lock, unlockTime, lockedAmount, owner, otherAccount };
+    return { lacat, owner, otherAccount };
   }
 
   describe("Deployment", function () {
-    it("Should set the right unlockTime", async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-
-      expect(await lock.unlockTime()).to.equal(unlockTime);
-    });
-
     it("Should set the right owner", async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture);
+      const { lacat, owner } = await loadFixture(deployLacat);
 
-      expect(await lock.owner()).to.equal(owner.address);
-    });
-
-    it("Should receive and store the funds to lock", async function () {
-      const { lock, lockedAmount } = await loadFixture(
-        deployOneYearLockFixture
-      );
-
-      expect(await ethers.provider.getBalance(lock.address)).to.equal(
-        lockedAmount
-      );
-    });
-
-    it("Should fail if the unlockTime is not in the future", async function () {
-      // We don't use the fixture here because we want a different deployment
-      const latestTime = await time.latest();
-      const Lock = await ethers.getContractFactory("Lock");
-      await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-        "Unlock time should be in the future"
-      );
+      expect(await lacat.owner()).to.equal(owner.address);
+      expect(await lacat.getDeposits()).to.be.empty;
     });
   });
 
-  describe("Withdrawals", function () {
-    describe("Validations", function () {
-      it("Should revert with the right error if called too soon", async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture);
+  describe("Deposit", function () {
+    it("Should revert if unlock time is not in future", async function () {
+      const { lacat } = await loadFixture(deployLacat);
 
-        await expect(lock.withdraw()).to.be.revertedWith(
-          "You can't withdraw yet"
-        );
-      });
+      const invalidUnlockTime = (await time.latest()) - 1;
 
-      it("Should revert with the right error if called from another account", async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // We can increase the time in Hardhat Network
-        await time.increaseTo(unlockTime);
-
-        // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-          "You aren't the owner"
-        );
-      });
-
-      it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(
-          deployOneYearLockFixture
-        );
-
-        // Transactions are sent using the first signer by default
-        await time.increaseTo(unlockTime);
-
-        await expect(lock.withdraw()).not.to.be.reverted;
-      });
+      await expect(lacat.deposit(invalidUnlockTime, { value: 1000 })).to.be.revertedWith(
+        "Lacat: Unlock time must be in future"
+      );
     });
 
-    describe("Events", function () {
-      it("Should emit an event on withdrawals", async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
+    it("Should deposit successfully", async function () {
+      const { lacat, otherAccount } = await loadFixture(deployLacat);
 
-        await time.increaseTo(unlockTime);
+      const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-        await expect(lock.withdraw())
-          .to.emit(lock, "Withdrawal")
-          .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-      });
+      await lacat.connect(otherAccount).deposit(unlockTime, { value: 1000 });
+
+      expect(await lacat.connect(otherAccount).getDeposits()).to.have.lengthOf(1);
     });
 
-    describe("Transfers", function () {
-      it("Should transfer the funds to the owner", async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-          deployOneYearLockFixture
-        );
+  });
 
-        await time.increaseTo(unlockTime);
+  describe("Withdrawal", function () {
+    it("Should transfer the funds to the owner", async function () {
+      const { lacat, owner } = await loadFixture(deployLacat);
 
-        await expect(lock.withdraw()).to.changeEtherBalances(
-          [owner, lock],
-          [lockedAmount, -lockedAmount]
-        );
-      });
+      const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+      const lockedAmount = 1000;
+
+      await lacat.deposit(unlockTime, { value: lockedAmount });
+
+      await time.increaseTo(unlockTime);
+
+      await expect(lacat.withdraw(1)).to.changeEtherBalances(
+        [owner, lacat],
+        [lockedAmount, -lockedAmount]
+      );
     });
   });
 });
