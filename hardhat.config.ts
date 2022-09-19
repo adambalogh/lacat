b9674d7709dd52445604e444596993c29e0e0039
Adam Balogh <balogh.adam@icloud.com>
2022-09-19 16:28:38 +0400

    goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     hardhat: {
       chainId: 1337
     },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${GOERLI_PRIVATE_KEY}`]
+    }
   }
 };
 
